703c6aef9771e112893185705ff1c21bd4de551a
Javier Gonzalez <javier.glez.martin@gmail.com>
2019-08-31 19:35:27 +0200

    Read elements from a DB

--- WpfDesktopContactsApp/MainWindow.xaml.cs
+++ WpfDesktopContactsApp/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WpfDesktopContactsApp.Classes;
 
 namespace WpfDesktopContactsApp
 {
@@ -23,12 +25,25 @@ namespace WpfDesktopContactsApp
         public MainWindow()
         {
             InitializeComponent();
+            ReadDatabase();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             NewContactWindow newContactWindow = new NewContactWindow();
             newContactWindow.ShowDialog();
+            // Execution of code will stop here and jump to the other window, until the user closes
+            // the windown newContactWindow, because we have called the method ShowDialog(), instead of just Show()
+            ReadDatabase();
+        }
+
+        private void ReadDatabase()
+        {
+            using (SQLiteConnection connection = new SQLiteConnection(App.databasePath))
+            {
+                connection.CreateTable<Contact>();
+                 var contacts = connection.Table<Contact>().ToList();  // we use var to define the variable contacts because we define and set the variable in the same line of code
+            }
         }
     }
 }
