e5543728aff36b50028c75b6c84aae12724b9e54
Javier Gonzalez <javier.glez.martin@gmail.com>
2019-08-31 17:12:27 +0200

    Save elements into a table of a DB, using the recently created Contacts class.

--- WpfDesktopContactsApp/NewContactWindow.xaml.cs
+++ WpfDesktopContactsApp/NewContactWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using WpfDesktopContactsApp.Classes;
 
 namespace WpfDesktopContactsApp
 {
@@ -26,7 +28,25 @@ namespace WpfDesktopContactsApp
 
         private void saveButton_Click(object sender, RoutedEventArgs e)
         {
-            //Save contact
+            // Save contact into the DB
+            // First, create an object Contact, with the data we want to store in passed to its attributes
+            Contact contact = new Contact()
+            {
+                Name = nameTextBox.Text,
+                Email = emailTextBox.Text,
+                Phone = phoneNumberTextbox.Text
+            };
+            // Second, create path to the db
+            string databaseName = "Contacts.db";
+            string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            string databasePath = System.IO.Path.Combine(folderPath, databaseName);
+            // Third, create connection to the DB, create a table that accepts contacts and store the contact in the table
+            SQLiteConnection connection = new SQLiteConnection(databasePath);
+            connection.CreateTable<Contact>();
+            connection.Insert(contact);
+            // Very important, CLOSE the connection to the DB at the end!!
+            connection.Close();
+
             Close();
         }
 
